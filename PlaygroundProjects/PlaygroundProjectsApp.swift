3d1b9e98c1e0ddc9abd447a1a27b548aa822e042
Damien Rosa <damien.rosa@quickline.net>
2024-12-10 09:58:40 +0100

    Initial Commit

---
+++ PlaygroundProjects/PlaygroundProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundProjectsApp.swift
+//  PlaygroundProjects
+//
+//  Created by Damien Rosa on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaygroundProjectsTests/PlaygroundProjectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundProjectsTests.swift
+//  PlaygroundProjectsTests
+//
+//  Created by Damien Rosa on 10/12/2024.
+//
+
+import Testing
+@testable import PlaygroundProjects
+
+struct PlaygroundProjectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
